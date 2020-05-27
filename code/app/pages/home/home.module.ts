56e7c0d3b6f1a96b613134e26ce27ecab567bad2
yangchunaijtt <1831127026@qq.com>
2020-05-27 13:22:23 +0800

    27

---
+++ code/app/pages/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { ShareModule } from 'src/app/share/share.module';
+import { HomeComponent } from './home.component';
+import { MemberCardComponent } from './components/member-card/member-card.component';
+
+
+@NgModule({
+  declarations: [HomeComponent, MemberCardComponent],
+  imports: [
+    HomeRoutingModule,
+    ShareModule
+  ]
+})
+// 太新路63号
+export class HomeModule {
+}
